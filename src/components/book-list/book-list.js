8a955c0ec8df322d51dc09f4ed1d6f55829f9831
xenleme <xenleme@gmail.com>
2019-04-02 08:40:57 +0300

    Dispatch action to the redux store

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -2,29 +2,43 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import BookListItem from '../book-list-item';
+import { withBookstoreService } from '../hoc';
+import { booksLoaded } from '../../actions';
+import { compose } from '../../utils';
 
 import './book-list.css';
 
 class BookList extends Component {
+  componentDidMount() {
+    const { bookstoreService } = this.props;
+    const data = bookstoreService.getBooks();
+
+    this.props.booksLoaded(data);
+  }
+
   render() {
     const { books } = this.props;
-
     return (
       <ul>
         {
           books.map((book) => {
             return (
-              <li key={book.id}><BookListItem book={book} /></li>
+              <li key={book.id}><BookListItem book={book}/></li>
             )
           })
         }
       </ul>
     );
-  };
-};
+  }
+}
 
 const mapStateToProps = ({ books }) => {
   return { books };
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = { booksLoaded };
+
+export default compose(
+  withBookstoreService(),
+  connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,5 @@
 const initialState = {
-  books: [
-    {
-      id: 1,
-      title: 'Eloquent JavaScript, 3rd Edition: A Modern Introduction to Programming',
-      author: 'Marijn Haverbeke'
-    },
-    {
-      id: 2,
-      title: 'Exploring ES6',
-      author: 'Axel Rauschmayer'
-    }
-  ]
+  books: []
 };
 
 const reducer = (state = initialState, action) => {
