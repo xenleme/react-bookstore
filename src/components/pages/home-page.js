78b7b9fa31abc3462dd6588c7c23ff955efd1224
xenleme <xenleme@gmail.com>
2019-04-02 07:23:25 +0300

    Render the dummy data on the home page

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import BookList from '../book-list';
 
 const HomePage = () => {
-  return <h2>home page</h2>
+  const books = [
+    {
+      id: 1,
+      title: 'Eloquent JavaScript, 3rd Edition: A Modern Introduction to Programming',
+      author: 'Marijn Haverbeke'
+    },
+    {
+      id: 2,
+      title: 'Exploring ES6',
+      author: 'Axel Rauschmayer'
+    }
+  ];
+
+  return (
+    <BookList books={books} />
+  );
 };
 
 export default HomePage;
